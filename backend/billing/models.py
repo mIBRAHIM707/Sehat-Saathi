3f89cbbffe0d823c9bea3ace2d994dddc2156a15
mIBRAHIM707 <ibrahimclash707@gmail.com>
2025-02-15 18:02:22 +0500

    bills

--- backend/billing/models.py
+++ backend/billing/models.py
@@ -1,3 +1,22 @@
+# billing/models.py
 from django.db import models
+from patients.models import Patient
+from appointments.models import Appointment
 
-# Create your models here.
+class Bill(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    appointment = models.ForeignKey(Appointment, on_delete=models.SET_NULL, null=True, blank=True)
+    bill_date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField(null=True, blank=True)
+    payment_method = models.CharField(max_length=255, blank=True)
+    STATUS_CHOICES = [
+        ('Paid', 'Paid'),
+        ('Unpaid', 'Unpaid'),
+        ('Overdue', 'Overdue'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Unpaid')
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Bill for {self.patient} on {self.bill_date}"
