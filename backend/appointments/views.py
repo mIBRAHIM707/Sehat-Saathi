59344dedaed4776d2e2e18513cec35c81ab472c7
mIBRAHIM707 <ibrahimclash707@gmail.com>
2025-02-15 19:21:59 +0500

    .

--- backend/appointments/views.py
+++ backend/appointments/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# appointments/views.py
+from django.shortcuts import render, get_object_or_404 #type: ignore
+from .models import Appointment
 
-# Create your views here.
+def appointment_list(request):
+    appointments = Appointment.objects.all()
+    return render(request, 'appointments/appointment_list.html', {'appointments': appointments})
+
+def appointment_detail(request, appointment_id):
+    appointment = get_object_or_404(Appointment, pk=appointment_id)
+    return render(request, 'appointments/appointment_detail.html', {'appointment': appointment})
--- backend/billing/views.py
+++ backend/billing/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# billing/views.py
+from django.shortcuts import render, get_object_or_404 # type: ignore
+from .models import Bill
 
-# Create your views here.
+def bill_list(request):
+    bills = Bill.objects.all()
+    return render(request, 'billing/bill_list.html', {'bills': bills})
+
+def bill_detail(request, bill_id):
+    bill = get_object_or_404(Bill, pk=bill_id)
+    return render(request, 'billing/bill_detail.html', {'bill': bill})
