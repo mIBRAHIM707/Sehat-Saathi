7565b5aecd7be2285f59d49b400aac6e1647f90a
mIBRAHIM707 <ibrahimclash707@gmail.com>
2025-02-17 00:53:49 +0500

    made models

--- backend/hms_backend/settings.py
+++ backend/hms_backend/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',  
     'corsheaders', 
+    'patients', 
+    'doctors', 
+    'appointments',
+    'billing',
 ]
 
 MIDDLEWARE = [
