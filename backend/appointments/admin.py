89a8bd5b46334ab629179ed58c0b715310f6c3de
mIBRAHIM707 <ibrahimclash707@gmail.com>
2025-03-07 02:46:08 +0500

    anew

--- backend/appointments/admin.py
+++
@@ -1,5 +0,0 @@
-# filepath: appointments/admin.py
-from django.contrib import admin
-from .models import Appointment
-
-admin.site.register(Appointment)
--- backend/billing/admin.py
+++
@@ -1,5 +0,0 @@
-# filepath: billing/admin.py
-from django.contrib import admin
-from .models import Billing
-
-admin.site.register(Billing)
